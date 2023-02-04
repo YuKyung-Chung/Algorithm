486076617ee6cc9447b0a6845693ca56d318f592
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-02-04 18:58:00 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 468 ms, Memory: 41088 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,25 +1,23 @@
 import java.io.*;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder answer = new StringBuilder();
 
-        while (t-- > 0) {
-            Stack<Character> stack = new Stack<>();
-            String str = br.readLine() +"\n";
-            for (char ch : str.toCharArray()) {
-                if (ch == ' ' || ch == '\n') {
-                    while (!stack.isEmpty()) {
-                        bw.write(stack.pop());
-                    }
-                    bw.write(ch);
-                }
-                else stack.push(ch);
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                StringBuilder sb = new StringBuilder(st.nextToken());
+                answer.append(sb.reverse()).append(" ");
             }
+            answer.append("\n");
         }
-        bw.flush();
+        System.out.println(answer);
+
     }
 }
