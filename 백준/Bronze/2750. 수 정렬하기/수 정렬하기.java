99fd99bbb41ecd5b12ca531c7b47a012de0fe29f
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-02-17 14:42:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 280 ms, Memory: 20796 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,26 +1,18 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
         int N = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0; i<N; i++){
+            int temp = sc.nextInt();
+            A[i] = temp;
         }
-
-        //버블 정렬
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp; 
-                }
-            }
+        Arrays.sort(A);
+        for(int i=0; i<N; i++){
+            sb.append(A[i]).append("\n");
         }
-        for (int i = 0; i < N; i++) {
-            System.out.println(arr[i]);
-        }
+        System.out.print(sb);
     }
 }
