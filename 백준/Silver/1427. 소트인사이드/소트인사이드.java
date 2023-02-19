338a31e6d7bc22d131f32dfdbfc8b175da43ea24
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-02-19 23:58:03 +0900

    [Silver V] Title: 소트인사이드, Time: 200 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,13 +1,12 @@
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char[] A = sc.nextLine().toCharArray();
-        Arrays.sort(A);
-        
-        for(int i=A.length-1; i>=0; i--){
-            System.out.print(A[i]);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        char[] list = sc.nextLine().toCharArray();
+        Arrays.sort(list);
+        for(int i=list.length-1; i>=0; i--){
+            System.out.print(list[i]);
         }
-        
     }
 }
