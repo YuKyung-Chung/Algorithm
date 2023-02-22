aff89bd9e8f11f2e211831214be977845fb15d03
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-02-22 14:30:37 +0900

    [Silver I] Title: 효율적인 해킹, Time: 8832 ms, Memory: 315124 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -8,53 +8,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-
-        answer = new int[N + 1];
-        A = new ArrayList[N + 1];
-        // A 배열 초기화
-        for (int i = 1; i <= N; i++) {
+        A = new ArrayList[N+1];
+        answer = new int[N+1];
+        for(int i=1; i<=N; i++){
             A[i] = new ArrayList<>();
         }
-
-        //인접 리스트 생성
-        for (int i = 0; i < M; i++) {
+        for(int i=0; i<M; i++){
             int S = sc.nextInt();
             int P = sc.nextInt();
             A[S].add(P);
         }
-
-        //신뢰도 배열 초기화
-        //모든 노드로 BFS 실행
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
+        
+       //모든 노드에서 BFS 실행
+        for(int i=1; i<=N;i++){
+            visited = new boolean[N+1];
             BFS(i);
         }
-
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, answer[i]);
+        int maxVal = 0;
+        for(int i=1; i<=N; i++){
+            maxVal = Math.max(maxVal, answer[i]);
         }
-        for (int i = 1; i <= N; i++) {
-            if (answer[i] == max) {
+        for(int i=1;i<=N; i++){
+            if(answer[i] == maxVal){
                 System.out.print(i + " ");
             }
         }
+        
     }
-
-    static void BFS(int node) {
+    static void BFS(int index){
         Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        visited[node] = true;
-
-        while (!q.isEmpty()) {
-            int temp = q.poll();
-            for (int i : A[temp]) {
-                if (!visited[i]) {
+        q.add(index);
+        visited[index] = true;
+        
+        while(!q.isEmpty()){
+            int now = q.poll();
+            for(int i:A[now]){
+                if(!visited[i]){
                     visited[i] = true;
-                    answer[i]++; //신규 노드 인덱스의 정답 배열 값 증가시킴
+                    answer[i]++;
                     q.add(i);
                 }
             }
         }
+        
     }
 }
