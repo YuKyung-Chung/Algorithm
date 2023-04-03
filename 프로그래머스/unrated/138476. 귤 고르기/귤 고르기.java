fbb42308934209be4046c92221bb3c19fc4b9df7
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-04-03 23:55:25 +0900

    [unrated] Title: 귤 고르기, Time: 181.91 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    Map<Integer, Integer> map = new TreeMap<>();
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        for(int i:tangerine){
+            map.put(i,map.getOrDefault(i,0) + 1);
+        }
+        //개수기준 정렬
+        List<Integer> keylist = new ArrayList<>(map.keySet());
+        Collections.sort(keylist, new customComparator());
+        
+        //정렬된 key 리스트에서 값을 하나씩 가져와 k에 빼줌
+        for(Integer e: keylist){
+            if(k <= 0)
+                break;
+            answer++;
+            k -= map.get(e);
+        }
+        return answer;
+    }
+    public class customComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2){
+            return map.get(o2).compareTo(map.get(o1));
+        }
+    }
+}
