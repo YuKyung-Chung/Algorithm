6a8222b0013faf0753f7ab469083fb81cb83b86d
정유경 <83561356+YuKyung-Chung@users.noreply.github.com>
2023-05-11 00:10:02 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2220 ms, Memory: 206104 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,43 +2,36 @@ import java.util.*;
 public class Main {
     static int N;
     static boolean[] visited;
-    static ArrayList<Integer>[] tree;
-    static int[] answer;
+    static ArrayList<Integer> tree[];
+    static int answer[];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-
         visited = new boolean[N + 1];
-        answer = new int[N + 1];
         tree = new ArrayList[N + 1];
-        //tree 초기화
+        answer = new int[N + 1];
+
         for (int i = 0; i < tree.length; i++) {
             tree[i] = new ArrayList<>();
         }
-
-        //인접 리스트 생성
         for (int i = 1; i < N; i++) {
-            int S = sc.nextInt();
-            int P = sc.nextInt();
-            tree[S].add(P);
-            tree[P].add(S);
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            tree[n1].add(n2);
+            tree[n2].add(n1);
         }
-        
-        DFS(1); //1번 노드부터 DFS 시작
-        
+
+        DFS(1);
         for (int i = 2; i <= N; i++) {
             System.out.println(answer[i]);
         }
     }
 
-    static void DFS(int node) {
-        visited[node] = true;
-
-        for (int i : tree[node]) {
+    static void DFS(int num) {
+        visited[num] = true;
+        for (int i : tree[num]) {
             if (!visited[i]) {
-                visited[i] = true;
-                //DFS를 탐색하면서 부모노드를 정답 배열에 저장
-                answer[i] = node; 
+                answer[i] = num; // 부모 노드를 정답 배열에 저장
                 DFS(i);
             }
         }
