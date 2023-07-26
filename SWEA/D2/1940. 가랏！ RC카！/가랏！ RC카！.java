bb08e4a1b56dbf0806fe2cb55e4f89c4a1b3258f
정유경 <jyk4523@gmail.com>
2023-07-26 15:06:31 +0900

    [D2] Title: 가랏! RC카!, Time: 141 ms, Memory: 20,940 KB -BaekjoonHub

---
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int k=1; k<=T; k++) {
+			int N = sc.nextInt();
+			int speed = 0; //초기 속도
+			int dist = 0; //초기 이동거리
+			
+			for(int i=0; i<N; i++) {
+				int a = sc.nextInt();
+				
+				if(a == 0) {
+					dist += speed;
+				} else if(a == 1) {
+					int b = sc.nextInt();
+					speed += b;
+					dist += speed;
+				} else if(a == 2) {
+					int b = sc.nextInt();
+					speed -= b;
+					if(speed < 0)
+						speed = 0;
+					dist += speed;
+				}
+			}
+			System.out.printf("#%d %d\n", k, dist);
+		}
+	}
+}
