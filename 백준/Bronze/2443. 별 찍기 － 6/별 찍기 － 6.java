8e481226ec6e0c626336fa60d5bc7a5ea39b2477
정유경 <jyk4523@gmail.com>
2023-07-30 19:43:22 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 396 ms, Memory: 18628 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j=2*N-1; j>=2*i-1; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
