8f940900003ec80273c94fe92a03cfba9cbe55fa
정유경 <jyk4523@gmail.com>
2023-09-04 17:01:25 +0900

    [Silver III] Title: N과 M (2), Time: 212 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,41 +1,47 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int N,M;
-    static boolean[] visit;
-    static int[] arr;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        arr = new int[M];
-        visit = new boolean[N];
-
-        dfs(0);
-
-    }
-
-    static void dfs(int idx) {
-        if (idx == M) {
-            for (int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true; //방문 여부 체크
-                arr[idx] = i + 1; //현재 인덱스에 값을 넣어주고
-                dfs(idx + 1); //다음 인덱스 채우기 위해 다시 재귀호출
-
-                //재귀호출이 끝난 후에 현재 i만 true로 바꾸고 나머지는 false로 해준다
-                for (int j = i+1; j < N; j++) {
-                    visit[j] = false;
-                }
-            }
-        }
-    }
+	static int N,M;
+	static int[] sel;
+	static boolean[] visited;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sel = new int[M];
+		sb = new StringBuilder();
+		visited = new boolean[N];
+		
+		perm(0,0);
+		System.out.println(sb);
+	} //main
+	
+	public static void perm(int idx, int sidx) {
+		//기저 부분
+		if(sidx == M) {
+			for (int val : sel) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		} //기저
+		
+		//재귀 부분
+		for (int i = 0; i < N; i++) {
+			if(visited[i]) continue;
+			
+			for (int j = 0; j <= i; j++) {
+				visited[j] = true;
+			}
+				
+			sel[sidx] = i+1;
+			perm(idx+1, sidx+1);
+			
+			for (int j = 0; j <= i; j++) {
+				visited[j] = false;
+			}
+		} //재귀
+	} //perm
 }
