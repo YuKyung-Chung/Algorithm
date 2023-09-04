55a363cd398cb6a70cd98a3287e68cf87f0a7ec5
정유경 <jyk4523@gmail.com>
2023-09-04 17:33:03 +0900

    [Silver III] Title: N과 M (3), Time: 1256 ms, Memory: 74928 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,33 +1,40 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    static int M,N;
-    static boolean[] visit;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        arr = new int[M];
-        visit = new boolean[N];
-
-        dfs(0);
-        System.out.print(sb);
-    }
-
-    static void dfs(int idx) {
-        if (idx == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(arr[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            arr[idx] = i + 1;
-            dfs(idx + 1);
-        }
-    }
+	static int N,M;
+	static boolean[] visited;
+	static int[] sel;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sel = new int[M];
+		visited = new boolean[N];
+		sb = new StringBuilder();
+		
+		perm(0,0);
+		System.out.println(sb);
+		
+	} //main
+	
+	public static void perm(int idx, int sidx) {
+		if(sidx == M) {
+			for (int val : sel){
+				sb.append(val).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		if(idx == M) return;
+		
+		
+		for (int i = 0; i < N; i++) {
+			sel[idx] = i+1;
+			perm(idx+1, sidx+1);
+			perm(idx+1, sidx);
+		}
+	} //perm
 }
