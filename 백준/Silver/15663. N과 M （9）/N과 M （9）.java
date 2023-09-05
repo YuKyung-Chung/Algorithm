ff50e87fb2d9e9115c00489a833332216b92099a
정유경 <jyk4523@gmail.com>
2023-09-06 01:15:16 +0900

    [Silver II] Title: N과 M (9), Time: 728 ms, Memory: 40316 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class Main {
+	static int N,M;
+	static int[] arr;
+	static int[] sel;
+	static boolean[] visited;
+	static LinkedHashSet<String> set;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		sel = new int[M];
+		visited = new boolean[N];
+		set = new LinkedHashSet<>();
+		
+		perm(0,0);
+		for(String val:set) {
+			System.out.println(val);
+		}
+	}//main
+	
+	public static void perm(int idx, int sidx) {
+		//기저부분
+		if(sidx == M) {
+
+			StringBuilder sb = new StringBuilder();
+			for (int val : sel) {
+				sb.append(val).append(" ");
+			}
+			set.add(sb.toString());
+			sb.append("\n");
+			return;
+		}
+		if(idx == M) return;
+						
+		//재귀부분
+		for (int i = 0; i < N; i++) {
+			if(visited[i]) continue;
+			
+			sel[idx] = arr[i];
+			visited[i] = true;
+			perm(idx+1, sidx+1);
+			visited[i] = false;
+		}
+		
+	}//perm
+}
