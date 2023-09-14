f37dc187b93662646b88a364c18f177fdd84b8c0
정유경 <jyk4523@gmail.com>
2023-09-14 16:45:10 +0900

    [Silver II] Title: DFS와 BFS, Time: 532 ms, Memory: 38220 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static int N,M,V,idx;
+	static int N,M,V;
 	static ArrayList<Integer>[] arr;
 	static boolean[] visited;
 	static StringBuilder dfs, bfs;
@@ -15,13 +15,13 @@ public class Main {
 		N = sc.nextInt(); //정점 개수
 		M = sc.nextInt(); //간선 개수
 		V = sc.nextInt(); //탐색 시작 정점 번호
+		arr = new ArrayList[N+1]; //간선 정보 저장할 배열
 		
-		arr = new ArrayList[N+1];
 		//초기화
 		for (int i = 0; i <= N; i++) {
 			arr[i] = new ArrayList<>();
 		}
-		//간선 정보 입력
+		//간선 정보 저장
 		for (int i = 0; i < M; i++) {
 			int u = sc.nextInt();
 			int v = sc.nextInt();
@@ -29,18 +29,20 @@ public class Main {
 			arr[u].add(v);
 			arr[v].add(u);
 		}
+		//오름차순 정렬
 		for (int i = 0; i <=N ; i++) {
 			Collections.sort(arr[i]);
 		}
 		
-		dfs = new StringBuilder();
-		visited = new boolean[N+1];
-		DFS(V);
+		dfs = new StringBuilder(); //dfs 방문순서 저장할 StringBuilder
+		visited = new boolean[N+1]; //visited 배열 초기화
+		DFS(V); //DFS 수행
 		
-		bfs = new StringBuilder();
-		visited = new boolean[N+1];
-		BFS(V);
+		bfs = new StringBuilder(); //dfs 방문순서 저장할 StringBuilder
+		visited = new boolean[N+1]; //visited 배열 초기화
+		BFS(V); //BFS 수행
 
+		//출력
 		System.out.println(dfs);
 		System.out.println(bfs);
 		
@@ -75,6 +77,5 @@ public class Main {
 				bfs.append(next+" ");
 			}
 		}
-		
 	}//BFS
 }
