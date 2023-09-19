4edfd5c7227281e98925d72655b84d8f7059dfb4
정유경 <jyk4523@gmail.com>
2023-09-19 23:54:13 +0900

    [Silver IV] Title: 스택, Time: 388 ms, Memory: 19188 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,73 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main{    
-    public static int stack[]; //스택 배열
-    public static int k=0; //스택 포인트
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine());
-
-        stack = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            switch (st.nextToken()) {
-                case "push":
-                    push(Integer.parseInt(st.nextToken()));
-                    break;
-                case "pop":
-                    sb.append(pop()).append('\n');
-                    break;
-                case "size":
-                    sb.append(size()).append('\n');
-                    break;
-                case "empty":
-                    sb.append(empty()).append('\n');
-                    break;
-                case "top":
-                    sb.append(top()).append('\n');
-                    break;
-            }
-        }
-        System.out.println(sb);
-    }
-
-    public static void push(int x) {
-        stack[k] = x;
-        k++;
-    }
-
-    public static int pop() {
-        if (k == 0) {
-            return -1;
-        } else {
-            int answer = stack[k-1];
-            k--;
-            return answer;
-        }
-    }
-
-    public static int size() {
-        return k;
-    }
-    
-    public static int empty() {
-        if (k == 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    public static int top() {
-        if (k == 0) {
-            return -1;
-        } else {
-            return stack[k-1];
-        } 
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String cmd = st.nextToken();
+			if(cmd.equals("push"))
+				stack.push(Integer.parseInt(st.nextToken()));
+			else if(cmd.equals("pop")){
+				if(stack.isEmpty())
+					System.out.println(-1);
+				else
+					System.out.println(stack.pop());
+			}
+			else if(cmd.equals("size"))
+				System.out.println(stack.size());
+			else if(cmd.equals("empty")){
+				if(stack.isEmpty())
+					System.out.println(1);
+				else
+					System.out.println(0);
+			}
+			else if(cmd.equals("top")){
+				if(stack.isEmpty())
+					System.out.println(-1);
+				else 
+					System.out.println(stack.peek());
+			}
+		}
+	}//main
 }
