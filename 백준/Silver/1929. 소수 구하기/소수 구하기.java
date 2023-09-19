188a5448d92bff45dd1308002b48190b1252727b
정유경 <jyk4523@gmail.com>
2023-09-20 00:07:41 +0900

    [Silver III] Title: 소수 구하기, Time: 512 ms, Memory: 25088 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,28 +1,36 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-        int[] A = new int[N + 1];
-        
-        for(int i=2;i<=N;i++){
-            A[i] = i;
-        }
-        //제곱근까지만 수행하기
-        for(int i=2; i<=Math.sqrt(N);i++){
-            if(A[i] == 0){
-                continue;
-            }
-            //배수 지우기
-            for(int j=i+i; j<=N; j=j+i){
-                A[j] = 0;
-            }
-        }
-        for(int i=M; i<=N; i++){
-            if(A[i] != 0){
-                System.out.println(A[i]);
-            }
-        }
-    }
+	static int M,N;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		M = sc.nextInt();
+		N = sc.nextInt();
+		sb = new StringBuilder();
+		
+		for (int i = M; i <= N; i++) {
+			isPrime(i);
+		}
+		
+		System.out.println(sb);
+	}//main
+	
+	public static void isPrime(int num) {
+		if(num == 1) return;
+		
+//		if(num == 2 || num == 3 || num == 5 || num == 7) {
+//			sb.append(num+"\n");
+//			return;
+//		}
+//		
+//		//1이거나 짝수인 경우 리턴
+//		if(num != 2 && num % 2 == 0) return;
+		
+		for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+			if(num % i == 0) return;
+		}
+		sb.append(num + "\n");
+	}
 }
