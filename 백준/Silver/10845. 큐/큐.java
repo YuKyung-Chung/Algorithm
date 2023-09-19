049663b9df53c0425326db2e2c2ef1e8b7df9bb7
정유경 <jyk4523@gmail.com>
2023-09-20 01:13:25 +0900

    [Silver IV] Title: 큐, Time: 356 ms, Memory: 19072 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -2,53 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        Queue<Integer> q = new LinkedList<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int last = 0;
-        int n = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            if (str.equals("push")) {
-                last = Integer.parseInt(st.nextToken());
-                q.add(last);
-            }
-            if (str.equals("pop")) {
-                if (q.isEmpty()) {
-                    sb.append(-1).append("\n");
-                } else {
-                    sb.append(q.poll()).append("\n");
-                }
-            }
-            if (str.equals("size")) {
-                sb.append(q.size()).append("\n");
-            }
-            if (str.equals("empty")) {
-                if (q.isEmpty() == true) {
-                    sb.append(1).append("\n");
-                } else {
-                    sb.append(0).append("\n");
-                }
-            }
-            if (str.equals("front")) {
-                if (q.isEmpty()) {
-                    sb.append(-1).append("\n");
-                } else {
-                    sb.append(q.peek()).append("\n");
-                }
-            }
-            if (str.equals("back")) {
-                if (q.isEmpty()) {
-                    sb.append(-1).append("\n");
-                } else {
-                    sb.append(last).append("\n");
-                }
-            }
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<>();
+		int last = 0;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String cmd = st.nextToken();
+			if(cmd.equals("push")) {
+				last =Integer.parseInt(st.nextToken());
+				q.add(last);
+			}
+			else if(cmd.equals("pop")){
+				if(q.isEmpty())
+					System.out.println(-1);
+				else
+					System.out.println(q.poll());
+			}
+			else if(cmd.equals("size"))
+				System.out.println(q.size());
+			else if(cmd.equals("empty")){
+				if(q.isEmpty())
+					System.out.println(1);
+				else
+					System.out.println(0);
+			}
+			else if(cmd.equals("front")){
+				if(q.isEmpty())
+					System.out.println(-1);
+				else 
+					System.out.println(q.peek());
+			}
+			else if(cmd.equals("back")) {
+				if(q.isEmpty())
+					System.out.println(-1);
+				else 
+					System.out.println(last);
+			}
+		}
+	}
 }
