e1b8ee50bcb5df2d7a21e726f134595b309cfa4f
정유경 <jyk4523@gmail.com>
2023-09-22 15:13:43 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 177 ms, Memory: 32,044 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,33 +1,45 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution {
-	private static final Object INF = Integer.MAX_VALUE;
-	static int N, min;
-	static int[][] arr;
+	static class Node implements Comparable<Node>{
+		int r; //행
+		int c; //열
+		int dist; //거리
+		
+		public Node(int r, int c, int dist){
+			this.r = r;
+			this.c = c;
+			this.dist = dist;
+		}
+		
+		//가중치 기준으로 오름차순 정렬
+		@Override
+		public int compareTo(Node o) {
+			return (this.dist > o.dist) ? 1 : -1;
+		}
+		
+	}
+	private static final int INF = Integer.MAX_VALUE;
+	static int N;
+	static Node[][] arr;
+	static int[][] dist;
 	static int[] dx = {1,-1,0,0};
 	static int[] dy = {0,0,1,-1};
-	static int[][] dist;
-		
 	
-	public static void main(String[] args)  {
+	public static void main(String[] args)   {
 		Scanner sc = new Scanner(System.in);
 		
 		int T = sc.nextInt(); //테스트 케이스 개수
 		for (int tc = 1; tc <= T; tc++) {
 			N = sc.nextInt();
-			arr = new int[N][N];
+			arr = new Node[N][N];
 			//배열 정보 저장
 			for (int i = 0; i < N; i++) {
 				String str = sc.next();
 				for (int j = 0; j < N; j++) {
-					arr[i][j] = Integer.parseInt(str.substring(j,j+1));
+					arr[i][j] =  new Node(i, j, Integer.parseInt(str.substring(j, j+1)));
 				}
 			}
 			dist = new int[N][N];
@@ -38,33 +50,26 @@ public class Solution {
 			}
 			
 			//다익스트라 알고리즘
-			PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
-
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					return o1[0] - o2[0];
-				}
-			});
-			q.add(new int[] {arr[0][0], 0, 0});
+			PriorityQueue<Node> q = new PriorityQueue<>();
+			q.add(new Node(0,0,0));
 			
 			while(!q.isEmpty()) {
-				int[] cur = q.poll();
+				Node cur = q.poll();
 					
 				for (int i = 0; i < 4; i++) {
-					int nx = dx[i] + cur[1];
-					int ny = dy[i] + cur[2];
+					int nx = dx[i] + cur.r;
+					int ny = dy[i] + cur.c;
 						
 					if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
 						
-					if(dist[nx][ny] > cur[0] + arr[nx][ny]) {
-						dist[nx][ny] = cur[0] + arr[nx][ny];
-						q.add(new int[] {dist[nx][ny], nx, ny});
+					if(dist[nx][ny] > cur.dist + arr[nx][ny].dist) {
+						dist[nx][ny] = cur.dist + arr[nx][ny].dist;
+						q.add(new Node(nx, ny, dist[nx][ny]));
 					}
 				}
-				if(cur[1] == N-1 && cur[2] == N-1) break;
+				if(cur.r == N-1 && cur.c == N-1) break;
 			}
 			System.out.printf("#%d %d\n",tc,dist[N-1][N-1]);
 		}//tc
 	}//main
-	
 }
