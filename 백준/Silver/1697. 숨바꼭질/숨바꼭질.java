705adfabb0505bb8865cb5af95fcd8ada9e92d6c
정유경 <jyk4523@gmail.com>
2023-09-27 12:07:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 244 ms, Memory: 24604 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,59 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int k;
-    static int[] visit = new int[100001];
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-
-        if (n == k) {
-            System.out.println(0);
-        } else {
-            bfs(n);
-        }
-
-
-    }
-
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visit[start] = 1; //방문횟수 1증가함
-
-        while (!q.isEmpty()) {
-            int temp = q.poll();
-
-            for (int i = 0; i < 3; i++) { //3가지 방법으로 움직일 수 있음
-                int next;
-
-                if (i == 0) {
-                    next = temp + 1;
-                } else if (i == 1) {
-                    next = temp - 1;
-                } else {
-                    next = temp * 2;
-                }
-
-                if (next == k) {
-                    System.out.println(visit[temp]);
-                    return;
-
-                }
-                if (next >= 0 && next<visit.length && visit[next] == 0) {
-                    q.add(next);
-                    visit[next] = visit[temp] + 1;
-                }
-            }
-
-        }
-
-
-    }
+	static int N,K,cnt;
+	static boolean[] visited;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt(); //수빈이 위치
+		K = sc.nextInt(); //동생 위치
+		visited = new boolean[300000];
+		
+		System.out.println(BFS(N));
+//		System.out.println(cnt);
+	}//main
+	
+	public static int BFS(int cur) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(cur);
+		visited[cur] = true;
+		
+		while(!q.isEmpty()) {
+			int a = q.size();
+			for (int i = 0; i < a; i++) {
+				int now = q.poll();
+//				System.out.println(cnt+"번 사이클"+now);
+				if(now == K) return cnt;
+				
+				if(now * 2 < K*2 && !visited[now*2]) {
+					q.add(now*2);
+					visited[now*2] = true;
+				}
+				if(now + 1 < K*2 && !visited[now+1]) {
+					q.add(now + 1);
+					visited[now+1] = true;
+				}
+				if(now -1 > 0 && !visited[now-1]) {
+					q.add(now - 1);
+					visited[now-1] = true;
+				}
+				
+			}
+			cnt++;
+		}
+		return cnt;
+	}
 }
