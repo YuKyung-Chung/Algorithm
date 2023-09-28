0d9d2a5e4162a32b49d274ee718700cd5f855178
정유경 <jyk4523@gmail.com>
2023-09-28 23:20:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 232 ms, Memory: 17820 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,74 +1,65 @@
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static int N;
-    public static int[][] arr;
-    public static boolean[][] vtd;
-    public static ArrayList<Integer> list;
-    public static int[] dx = {-1, 0, 1, 0};
-    public static int[] dy = {0, -1, 0, 1};
-    public static void solve(int pp, int qq) {
-        Queue<int[]> q = new LinkedList<>();
-        vtd[pp][qq] = true; //방문 처리
-        q.add(new int[]{pp, qq});
-
-        int size = 1;
-        //q에 값이 있으면
-        while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int x = tmp[0];
-            int y = tmp[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N || arr[nx][ny] == 0 || vtd[nx][ny]) {
-                    continue;
-                } else {
-                    vtd[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
-                    size++;
-                }
-
-            }
-        }
-        list.add(size);
-    }
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); //N x N
-        arr = new int[N][N];
-        vtd = new boolean[N][N];
-        list = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Character.getNumericValue(str.charAt(j));
-            }
-        }
-
-        int count = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                //아직 방문되지 않았고, 1인 경우
-                if (!vtd[i][j] && arr[i][j] == 1) {
-                    count++;
-                    solve(i, j); //방문처리
-                }
-            }
-        }
-        System.out.println(count);
-        Collections.sort(list);
-        StringBuilder sb = new StringBuilder();
-        for (Integer integer : list) {
-            sb.append(integer).append("\n");
-        }
-        System.out.println(sb.toString());
-    }
+	static int N, cnt, town;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt(); //지도 크기
+		map = new int[N][N];
+		
+		//지도 정보 입력받기
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(str.substring(j,j+1));
+			}
+		}
+		List<Integer> list = new ArrayList<>();
+		visited = new boolean[N][N];
+		town = 0;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(!visited[i][j] && map[i][j] == 1) {
+					cnt=1;
+					town++;
+					DFS(i,j);
+					list.add(cnt);
+				}
+			}
+		}
+		System.out.println(town);
+		Collections.sort(list);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	} //main
+	
+	public static void DFS(int x, int y) {
+		visited[x][y] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int nx = dx[i] + x;
+			int ny = dy[i] + y;
+			
+			//edge case
+			if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] == 0) continue;
+			
+			if(!visited[nx][ny] && map[nx][ny] == 1) {
+				visited[nx][ny] = true;
+				cnt++;
+				DFS(nx,ny);
+			}
+			
+		}
+		
+	}//DFS
 }
