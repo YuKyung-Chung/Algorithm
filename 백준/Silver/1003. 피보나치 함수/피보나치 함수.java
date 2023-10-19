0c0fae74fff696e0cf3bfaa80ace1139963a6c1e
정유경 <jyk4523@gmail.com>
2023-10-19 19:08:36 +0900

    [Silver III] Title: 피보나치 함수, Time: 240 ms, Memory: 18592 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[][] dp = new int[N+1][2];
+			
+			dp[0][0] = 1;
+			dp[0][1] = 0;
+			if(N>=1) {
+				dp[1][0] = 0;
+				dp[1][1] = 1;
+				for (int i = 2; i <= N; i++) {
+					for (int j = 0; j < 2; j++) {
+						dp[i][j] = dp[i-1][j] + dp[i-2][j];
+					}
+				}
+			}
+			System.out.println(dp[N][0] + " " + dp[N][1]);
+		}//tc
+	}//main
+}
