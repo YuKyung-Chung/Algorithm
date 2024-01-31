6a47e3a9a34e47c8e78632271b4495a405ccd5e6
정유경 <jyk4523@gmail.com>
2024-01-31 10:33:44 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1348 ms, Memory: 79140 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,39 +1,50 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    static int N;
-    static boolean[] visited;
-    static ArrayList<Integer> tree[];
-    static int answer[];
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        visited = new boolean[N + 1];
-        tree = new ArrayList[N + 1];
-        answer = new int[N + 1];
+	static int N; //노드 개수
+	static int[] parent; //부모노드 저장
+	static boolean[] isVisit;
+	static ArrayList<Integer> list[]; //노드 저장
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		isVisit = new boolean[N+1];
+		list = new ArrayList[N+1];
+		parent = new int[N+1];
+		
+		for (int i = 0; i < N+1; i++) {
+			list[i] = new ArrayList<>();
+		}
 
-        for (int i = 0; i < tree.length; i++) {
-            tree[i] = new ArrayList<>();
-        }
-        for (int i = 1; i < N; i++) {
-            int n1 = sc.nextInt();
-            int n2 = sc.nextInt();
-            tree[n1].add(n2);
-            tree[n2].add(n1);
-        }
-
-        DFS(1);
-        for (int i = 2; i <= N; i++) {
-            System.out.println(answer[i]);
-        }
-    }
-
-    static void DFS(int num) {
-        visited[num] = true;
-        for (int i : tree[num]) {
-            if (!visited[i]) {
-                answer[i] = num; // 부모 노드를 정답 배열에 저장
-                DFS(i);
-            }
-        }
-    }
+		StringTokenizer st;
+		for (int i = 0; i < N-1; i++) {
+			//연결된 노드의 값 입력받기
+			st= new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			list[a].add(b);
+			list[b].add(a);
+		}
+		
+		DFS(1); //트리 루트 1
+		
+		for (int i = 2; i < parent.length; i++) {
+			System.out.println(parent[i]);
+		}
+		
+	}//main
+	
+	public static void DFS(int index) {
+		isVisit[index] = true;
+		for (int i : list[index]) {
+			if(!isVisit[i]) {
+				parent[i] = index;
+				DFS(i);
+			}
+		}
+	}
 }
