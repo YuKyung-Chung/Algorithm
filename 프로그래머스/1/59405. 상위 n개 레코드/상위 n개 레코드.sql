6aaf352bb69d004a66f8ddfe838b9b8f3e8a9edc
정유경 <jyk4523@gmail.com>
2024-07-08 19:08:24 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,6 +1,8 @@
--- 코드를 입력하세요
-SELECT NAME
-FROM ANIMAL_INS
-ORDER BY DATETIME ASC
-LIMIT 1
-;
+SELECT name
+FROM 
+(
+    SELECT name
+    FROM animal_ins
+    ORDER BY datetime
+)
+WHERE rownum = 1
