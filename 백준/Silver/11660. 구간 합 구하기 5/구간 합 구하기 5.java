cd5bec266e19b3bc12472890c0d5cf1bd39392d3
정유경 <jyk4523@gmail.com>
2024-09-24 17:37:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2636 ms, Memory: 327464 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,41 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[N+1][N+1];
-        //원본 배열 저장하기
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        int[][] D = new int[N+1][N+1];
-        //합 배열 저장하기
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + arr[i][j];
-            }
-        }
-        
-        int answer = 0;
-        //질의 계산 및 출력하기
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-
-            answer = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-            System.out.println(answer);
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); //표의크기
+		int M = sc.nextInt(); //합 구해야 하는 횟수
+		
+		int[][] arr = new int[N+1][N+1];
+		int[][] S = new int[N+1][N+1]; //구간합 저장할 배열
+		//배열 값 채우기
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				arr[i][j] = sc.nextInt();
+				S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + arr[i][j];
+				
+			}
+		}
+		
+		for(int i=0; i<M; i++) {
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			
+			System.out.println(S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1]);
+		}
+	}
 }
