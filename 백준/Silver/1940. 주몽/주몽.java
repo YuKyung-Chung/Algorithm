31890eb106adcd4f4f30438f388902248df06339
정유경 <jyk4523@gmail.com>
2024-09-24 18:29:36 +0900

    [Silver IV] Title: 주몽, Time: 408 ms, Memory: 34324 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,33 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-
-        int count = 0;
-        int start = 0;
-        int end = N-1;
-        while (start < end) {
-            if (arr[start] + arr[end] < M) {
-                start++;
-            } else if (arr[start] + arr[end] > M) {
-                end--;
-            } else {
-                count++;
-                start++;
-                end--;
-            }
-        }
-        System.out.println(count);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();//재료개수
+		int M = sc.nextInt(); //필요한 수
+		int[] in = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			in[i] = sc.nextInt();
+		}
+		Arrays.sort(in); //오름차순 정렬
+		
+		int start = 0;
+		int end = in.length-1;
+		int count = 0;
+		int sum = in[start] + in[end];
+		
+		while(start != end) {
+			if(sum == M) {
+				count++;
+				start++;
+				sum = in[start] + in[end];
+			}
+			else if(sum < M) {
+				sum = sum - in[start];
+				start++;
+				sum = in[start] + in[end];
+			} else if(sum > M) {
+				sum = sum - in[end];
+				end--;
+				sum = sum + in[end];
+			}
+		}
+		System.out.println(count);
+		
+	}
 }
