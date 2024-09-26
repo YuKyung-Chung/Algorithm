12bd312b34099bf08a48a8351d75d2a7b3583690
정유경 <jyk4523@gmail.com>
2024-09-26 18:39:33 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,33 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+	static int[][] arr = new int[101][101];
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int T = sc.nextInt(); //총 색종이 수
-		int[][] arr = new int[101][101];
-		int count = 0; //넓이 저장 변수
+		int N = sc.nextInt(); //색종이 수
 		
-		//arr 배열 0으로 초기화
-		for(int i=0; i<101; i++) {
-			for(int j=0; j<101; j++) {
-				arr[i][j] = 0;
+		for(int i=0; i<N; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			checkArea(x,y);
+		}
+		
+		int area = 0; //겹치는 부분 카운트 변수
+		for(int i=0; i<100; i++) {
+			for(int j=0; j<100; j++) {
+				if(arr[i][j] == 1)
+					area++;
 			}
 		}
 		
-		
-		for(int k=0; k<T; k++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			for(int i=a; i<a+10; i++) {
-				for(int j=b; j<b+10; j++) {
-					if(arr[i][j] != 1) {
-						arr[i][j] = 1;
-						count++;
-					}
-				}
+		System.out.println(area);
+	}//end of main
+	
+	//겹치는 영역 체크
+	public static void checkArea(int x, int y) {
+		for(int i = x; i < x+10; i++) {
+			for(int j = y; j < y+10; j++) {
+				arr[i][j] = 1;
 			}
 		}
-		System.out.println(count);
 	}
 }
