cc2dab436f759863170f4366459a95cefb4af80d
정유경 <jyk4523@gmail.com>
2024-10-16 01:27:41 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 331.13 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 136 MB, 시간: 179.41 ms
+메모리: 130 MB, 시간: 331.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 19일 16:19:54
+2024년 10월 16일 01:27:39
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,25 +1,31 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        //중복되는 값 제거하기 위한 Set
-        HashSet<Integer> set = new HashSet<>();
-        int N = elements.length;
+        int n = elements.length;
+        Set<Integer> set = new HashSet<>();
         
-        //원형 수열이므로 기존 수열의 2배로 이어붙임
-        int[] extended = new int[N * 2];
-        for(int i=0; i<N; i++){
-            extended[i] = elements[i];
-            extended[i+N] = elements[i];
+        int[] circle_elements = new int[n*2];
+        for(int i=0; i<n; i++){
+            circle_elements[i] = elements[i];
+        }
+        for(int i=n; i<circle_elements.length; i++){
+            circle_elements[i] = elements[i-n];
         }
         
-        for(int i=0; i<N; i++){
-            int sum = 0;
-            for(int j=i; j<i+N; j++){
-                sum += extended[j];
+        //길이 1부터 n까지의 모든 연속 부분 수열 합 계산
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<n; j++){
+                int sum  = 0;
+                //부분 수열 합 계산
+                for(int k=0; k<i; k++){
+                    sum += circle_elements[j+k];
+                }
                 set.add(sum);
             }
         }
+        
         
         return set.size();
     }
