74db9fc018a293a771f6a9e043dca146ef9422cd
정유경 <jyk4523@gmail.com>
2024-10-18 14:04:15 +0900

    [level 2] Title: 더 맵게, Time: 1623.84 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,41 +1,25 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    static PriorityQueue<Integer> pq;
-    static int answer = 0;
-    static boolean finish = false;
-    
     public int solution(int[] scoville, int K) {
-        pq = new PriorityQueue<>();
-        
-        for(int i : scoville){
-            pq.offer(i);
-        }
-        makeSpicy(K);
-        
-        if(finish == false) return -1;
-        else return answer;
-    }//end of solution
-    private static void makeSpicy(int K){
-        
-        while(pq.size() >= 2){
-            int now = pq.poll();
-            int second = pq.poll();
-            if(now >= K){
-                finish = true;
-                return;
-            }
-            
-            int after = now + (second * 2);
-            answer += 1;
-            pq.offer(after);
-            
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //오름차순으로 자동 정렬
+        for(int i: scoville){
+            pq.add(i);
         }
         
-        if(pq.poll() >= K){
-            finish = true;
-        } 
-        return;
-    }//end of makeSpicy
+        //가장 작은 원소가 K 미만일 경우
+        while(pq.peek() < K && pq.size() >= 2){
+            pq.add(pq.poll() + (pq.poll() * 2));
+            answer++;
+        }
+        
+        //마지막 남은 원소가 K 미만인지 확인
+        if(pq.size() < 2 && pq.peek() < K){
+            answer = -1;
+        }
+        
+        return answer;
+    }
 }
