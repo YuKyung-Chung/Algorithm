58fcaba5d58496982c4b9cef78ee351d02eccdc3
정유경 <jyk4523@gmail.com>
2024-10-18 16:39:05 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 서울에 위치한 식당 목록 출력하기 - 131118 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 07월 08일 17:20:30
+2024년 10월 18일 16:39:03
 
 ### 문제 설명
 
--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,11 +1,9 @@
-with avg_score as(
-    select rest_id, avg(nvl(REVIEW_SCORE,0)) as score
-    from REST_REVIEW
-    group by rest_id
-)
 -- 코드를 입력하세요
-SELECT r.rest_id, r.rest_name, r.food_type, r.favorites, r.address, round(a.score,2) as score
-from REST_INFO r ,avg_score a 
-where r.rest_id =  a.rest_id
-and address like '서울%'
-order by a.score desc, r.FAVORITES desc
+SELECT A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS, ROUND(AVG(B.REVIEW_SCORE),2) AS SCORE
+FROM REST_INFO A
+JOIN REST_REVIEW B
+ON A.REST_ID = B.REST_ID
+WHERE A.ADDRESS LIKE '서울%'
+GROUP BY A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS
+ORDER BY SCORE DESC, FAVORITES DESC
+;
