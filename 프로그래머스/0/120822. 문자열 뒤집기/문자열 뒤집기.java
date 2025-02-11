1fdb0a973414ec52efceb5c859ba5ac5b1b8be9e
정유경 <jyk4523@gmail.com>
2025-02-11 23:30:27 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(my_string).reverse().toString();
+    }
+}
