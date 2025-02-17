9cf01b2580c452716925a1cc41bafc21aa4cd9e6
정유경 <jyk4523@gmail.com>
2025-02-17 23:21:35 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -4,12 +4,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         int sqrt = (int) Math.sqrt(n);
-        if(sqrt * sqrt == n){
-            answer = 1;
-        }
-        else{
-            answer = 2;
-        }
+        answer = (sqrt * sqrt == n)? 1: 2;
         
         return answer;
     }
