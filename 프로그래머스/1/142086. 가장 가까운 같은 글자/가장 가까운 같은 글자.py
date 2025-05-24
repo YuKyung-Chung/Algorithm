0f70ac9b598177c63db913c270dc12a4ea3b7c31
정유경 <jyk4523@gmail.com>
2025-05-24 23:57:18 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.40 ms, Memory: 9.91 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,12 @@
+def solution(s):
+    last_seen = {}  # 문자의 마지막 위치를 저장
+    result = []
+
+    for i, char in enumerate(s):
+        if char in last_seen:
+            result.append(i - last_seen[char])
+        else:
+            result.append(-1)
+        last_seen[char] = i  # 현재 위치로 업데이트
+
+    return result
