a070fb80b8ad1f84daba54dfdac93c7983a05a65
정유경 <jyk4523@gmail.com>
2025-11-06 23:39:42 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.52 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 2.73 ms
+메모리: 9.98 MB, 시간: 1.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 25일 17:31:01
+2025년 11월 06일 23:39:39
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,13 +1,11 @@
 def solution(s):
     last_seen = {}  # 문자의 마지막 위치를 저장
-    result = []
-
-    for i in range(len(s)):
-        char = s[i] # 현재 위치의 문자
+    result= []
+    
+    for i, char in enumerate(s):
         if char in last_seen:
-            result.append(i - last_seen[char]) #현재 인덱스 - 이전 등장 인덱스
+            result.append(i-last_seen[char])
         else:
-            result.append(-1) #처음 나온 문자
-        last_seen[char] = i # 현재 인덱스 기록
-
+            result.append(-1)
+        last_seen[char] = i # 현재 위치로 업데이트
     return result
