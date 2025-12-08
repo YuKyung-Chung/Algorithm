764ab31969b578d9a8786b17f9bf37f8ffc92ed9
정유경 <jyk4523@gmail.com>
2025-12-08 15:33:49 +0900

    [level 2] Title: 숫자의 표현, Time: 1.98 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,26 @@
+# 투포인터 알고리즘
 def solution(n):
-    answer = 0
+    start, end = 1, 1
+    current_sum = 1
+    count = 0
     
-    for start in range(1, n+1):
-        total = 0
-        while total < n:
-            total += start
+    while start <= n:
+        if current_sum == n:
+            # n 만드는 하나의 방법 발견
+            count += 1
+            # 구간 오른쪽으로 하나 밀기
+            current_sum -= start
             start += 1
-            if total == n:
-                answer += 1
-                break
+        elif current_sum < n:
+            # end 하나 늘려서 더 큰 수 포함하기
+            end += 1
+            current_sum += end
+        else:
+            # 합이 크면 start 하나 늘려서 왼쪽 줄이기
+            current_sum -= start
+            start += 1
+            
+    return count
+            
     
-    
-    return answer
+    return count
