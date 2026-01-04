ba2d8061dccb5b864df2d60fb56727ccf417c683
정유경 <jyk4523@gmail.com>
2026-01-04 17:41:24 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.13 MB, 시간: 0.07 ms
+메모리: 9.37 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 11일 23:27:15
+2026년 01월 04일 17:41:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,26 @@
-from collections import deque
 import math
 
 def solution(progresses, speeds):
     answer = []
-    release = [] # 배포되는 기간 저장
-
+    # 완료되기까지 걸리는 날짜
+    complete_date = [0] * len(progresses)
     for i in range(len(progresses)):
-        release.append(math.ceil((100-progresses[i]) / speeds[i]))
+        complete_date[i] = math.ceil((100 - progresses[i])/speeds[i])
     
-    current_day = release[0]
-    q = deque()
-    q.append(current_day)
-    
-    for days in release[1:]:
-        if days <= current_day :
-            q.append(days)
+    count = 1
+    current = complete_date[0]
+    for i in range(1, len(complete_date)):
+        # current = max(current, complete_date[i])
+        
+        if complete_date[i] <= current:
+            count += 1
         else:
-            answer.append(len(q))
-            q.clear()
-            current_day = days
-            q.append(days)
-            
-    answer.append(len(q))
-
+            # 크면 answer.append(cnt) 하고 current = d, count = 1
+            answer.append(count)
+            count = 1
+            current = complete_date[i]
+    
+    # 마지막
+    answer.append(count)
     
     return answer
