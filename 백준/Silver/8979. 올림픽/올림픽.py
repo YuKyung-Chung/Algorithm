2500ccd770269b84454f2ab4d679aafc4cf005a2
정유경 <jyk4523@gmail.com>
2026-02-05 18:06:12 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -7,7 +7,8 @@ countries.sort(key=lambda c: (-c[1], -c[2], -c[3]))
 rank = [0 for _ in range(N+1)]
 rank[countries[0][0]] = 1
 for c in range(1,len(countries)):
-    if(countries[c][1:3] == countries[c-1][1:3]):
+    # 금/은/동 모두 같으면 공동 순위
+    if(countries[c][1:] == countries[c-1][1:]):
         rank[countries[c][0]] = rank[countries[c-1][0]]
     else:
         rank[countries[c][0]] = rank[countries[c-1][0]] + 1
