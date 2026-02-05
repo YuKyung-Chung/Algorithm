92b4a1f37cd6424996a971edc9510995e61c604e
정유경 <jyk4523@gmail.com>
2026-02-05 18:02:12 +0900

    [Silver V] Title: 올림픽, Time: 32 ms, Memory: 32412 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+countries = []
+for _ in range(N):
+    countries.append(list(map(int, input().split())))
+
+countries.sort(key=lambda c: (-c[1], -c[2], -c[3]))
+rank = [0 for _ in range(N+1)]
+rank[countries[0][0]] = 1
+for c in range(1,len(countries)):
+    if(countries[c][1:3] == countries[c-1][1:3]):
+        rank[countries[c][0]] = rank[countries[c-1][0]]
+    else:
+        rank[countries[c][0]] = rank[countries[c-1][0]] + 1
+
+print(rank[K])
