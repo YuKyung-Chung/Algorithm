48b25dc6977606630041d42be0f5f0797b270c5f
정유경 <jyk4523@gmail.com>
2026-02-05 18:13:16 +0900

    [Silver V] Title: 올림픽, Time: 56 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -4,13 +4,13 @@ for _ in range(N):
     countries.append(list(map(int, input().split())))
 
 countries.sort(key=lambda c: (-c[1], -c[2], -c[3]))
-rank = [0 for _ in range(N+1)]
+rank = [0] * (N+1)
 rank[countries[0][0]] = 1
-for c in range(1,len(countries)):
+for c in range(1,N):
     # 금/은/동 모두 같으면 공동 순위
     if(countries[c][1:] == countries[c-1][1:]):
         rank[countries[c][0]] = rank[countries[c-1][0]]
     else:
-        rank[countries[c][0]] = rank[countries[c-1][0]] + 1
+        rank[countries[c][0]] = c + 1
 
 print(rank[K])
