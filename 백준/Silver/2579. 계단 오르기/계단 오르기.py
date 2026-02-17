7067e4ad9a030cdcc36a5ac9e528d13ce9fc3f8a
정유경 <jyk4523@gmail.com>
2026-02-17 17:51:44 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -5,7 +5,6 @@ for i in range(1, N+1):
 
 dp = [[0]*3 for _ in range(N+1)]
 dp[1][1] = stairs[1] # 첫번째 계단 밟은 경우
-dp[1][0] = 0 # 첫번째 계단 안밟은 경우
 
 if N>=2:
     dp[2][1] = stairs[2] # 두번째 계단 밟은 경우
@@ -14,7 +13,7 @@ if N>=2:
 if N>=3:
     for j in range(3, N+1):
         dp[j][2] = dp[j-1][1] + stairs[j]
-        dp[j][1] = max(dp[j-2][1] + stairs[j], dp[j-2][2]+ stairs[j], dp[j-2][0]+ stairs[j])
+        dp[j][1] = max(dp[j-2][1] + stairs[j], dp[j-2][2]+ stairs[j])
 dp[N][0] = max(dp[N][1], dp[N][2])
 
 print(dp[N][0])
