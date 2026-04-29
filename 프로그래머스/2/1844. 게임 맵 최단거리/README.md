fd04cf913c880a91ecea16253dfce3eec5523790
정유경 <jyk4523@gmail.com>
2026-04-29 23:51:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.39 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 11.83 ms
+메모리: 9.37 MB, 시간: 10.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 08일 03:01:20
+2026년 04월 29일 23:51:28
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,37 +1,32 @@
 from collections import deque
-
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    answer = 0
+    cnt = 0
+    N = len(maps)
+    M = len(maps[0])
     
-    # 방향: 동, 서, 남, 북
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
     
-    def bfs(x, y):
-        queue = deque()
-        queue.append((x, y))
+    q = deque([(0,0,1)])
+    visited = [[False]*M for _ in range(N)]
+    visited[0][0] = True
+    
+    while q:
+        x,y,dist = q.popleft()
         
-        while queue:
-            x, y = queue.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                
-                # 범위 벗어나면 무시
-                if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                    continue
-                
-                # 벽이거나 이미 방문한 칸이면 무시
-                if maps[nx][ny] == 0 or maps[nx][ny] != 1:
-                    continue
-                
-                # 최단 거리 누적 저장
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx, ny))
+        # 도착하면 거리반환
+        if x == N-1 and y == M-1:
+            return dist
         
-        # 도착지에 도달 못했으면 여전히 1일 것이므로 예외 처리 필요
-        return maps[n - 1][m - 1]
-
-    result = bfs(0, 0)
-    return result if result != 1 else -1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < N and 0 <= ny < M:
+                if not visited[nx][ny] and maps[nx][ny] == 1:
+                    visited[nx][ny] = True
+                    q.append((nx, ny, dist+1))
+                    
+        
+    return -1
