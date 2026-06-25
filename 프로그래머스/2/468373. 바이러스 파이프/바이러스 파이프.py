c0db6e7b60e49480cb8027a57a73925c42999882
정유경 <jyk4523@gmail.com>
2026-06-26 00:40:00 +0900

    [level 2] Title: 바이러스 파이프, Time: 11.85 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
@@ -0,0 +1,48 @@
+from itertools import product
+
+def solution(n, infection, edges, k):
+    answer = 0
+    
+    graph = [[] for _ in range(n+1)]
+    
+    for x,y,pipe_type in edges:
+        graph[x].append((y,pipe_type))
+        graph[y].append((x,pipe_type))
+    
+    for length in range(1, k+1):
+        for order in product([1,2,3], repeat=length):
+            
+            infected = set()
+            infected.add(infection)
+            
+            for pipe_type in order:
+                from itertools import product
+
+def solution(n, infection, edges, k):
+    answer = 0
+    
+    graph = [[] for _ in range(n+1)]
+    
+    for x,y,pipe_type in edges:
+        graph[x].append((y,pipe_type))
+        graph[y].append((x,pipe_type))
+    
+    for length in range(1, k+1):
+        for order in product([1,2,3], repeat=length):
+            
+            infected = set()
+            infected.add(infection)
+            
+            for pipe_type in order:
+                # 현재 감염된 노드들에서 시작해서 pipe_type과 같은 파이프만 타고 갈 수 있는 노드들을 전부 감염시키기
+                stack = list(infected)
+                while stack:
+                    now = stack.pop()
+                    for next_node, next_type in graph[now]:
+                        # 지금 연 파이프 타입과 같아야 함 & 아직 감염되지 않은 노드
+                        if next_type == pipe_type and next_node not in infected:
+                            infected.add(next_node)
+                            stack.append(next_node)
+            answer = max(answer, len(infected))
+
+    return answer
