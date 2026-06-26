6a3e52c13d0745ff886185af94b1512d15abf9c1
정유경 <jyk4523@gmail.com>
2026-06-26 14:31:03 +0900

    [level 1] Title: 폰켓몬, Time: 0.56 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,15 +1,16 @@
 from collections import Counter
 def solution(nums):
     answer = 0
+    N = len(nums)//2 # 선택할 수 있는 폰켓몬 수
     
-    # 고를수있는 폰켓몬수
-    N = len(nums)/2
-    poketmons = Counter(nums)
+    c = Counter(nums)
     
-    if len(poketmons) > N:
+    # 선택할 수 있는 가짓수가 전체 수보다 많으면 N이 정답
+    if len(c) >= N:
         answer = N
+    # 그게 아닌경우 
     else:
-        answer = len(poketmons)
+        answer = len(c)
+    
     
     return answer
-
