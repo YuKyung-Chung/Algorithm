9c5797f6b73a7424bb53c3726cb816d59dfd3930
정유경 <jyk4523@gmail.com>
2026-06-26 16:26:39 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,23 @@
 import math
+from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    # 완료되기까지 걸리는 날짜
-    complete_date = [0] * len(progresses)
-    for i in range(len(progresses)):
-        complete_date[i] = math.ceil((100 - progresses[i])/speeds[i])
+    progress_days = [] # 작업 후 배포 가능일수
     
-    count = 1
-    current = complete_date[0]
-    for i in range(1, len(complete_date)):
-        # current = max(current, complete_date[i])
-        
-        if complete_date[i] <= current:
+    for p, s in zip(progresses, speeds):
+        progress_days.append(math.ceil((100-p) / s))
+    
+    now_max= progress_days[0] # 현재 최대 작업길이
+    count = 0
+    for day in progress_days:
+        # 현재 작업일이 최대 작업길이 보다 작으면 배포일에 같이 배포
+        if day <= now_max:
             count += 1
         else:
-            # 크면 answer.append(cnt) 하고 current = d, count = 1
             answer.append(count)
             count = 1
-            current = complete_date[i]
-    
-    # 마지막
-    answer.append(count)
-    
+            now_max = day
+            
+    answer.append(count) # 마지막 배포 묶음 추가
     return answer
