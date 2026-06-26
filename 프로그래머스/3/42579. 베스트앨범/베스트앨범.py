967d8772598ac55c8764c3999f32c1340d63e1f3
정유경 <jyk4523@gmail.com>
2026-06-26 15:46:57 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -3,20 +3,17 @@ def solution(genres, plays):
     answer = []
     g_dict = defaultdict(lambda: [0, []])
     
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        g_dict[g][0] += p # 총 재생수
-        g_dict[g][1].append((i,p)) # (인덱스, 재생수)
+    for i, (genre, play) in enumerate(zip(genres, plays)):
+        g_dict[genre][0] += play # 총 재생수
+        g_dict[genre][1].append((i, play)) # 인덱스, 재생수
     
-    # 속한 노래가 많이 재생된 장르 먼저 수록
-    sorted_genres = sorted(g_dict.items(), key=lambda x: x[1][0], reverse = True)
-    print(sorted_genres)
-    for genre, (total, songs) in sorted_genres:
-        # 장르 내에서 많이 재생된 노래 먼저 수록
-        # 장르 내에서 재생 횟수 같은 노래 중에서 고유 번호 낮은 노래 먼저 수록
-        sorted_songs = sorted(songs, key=lambda x:(-x[1], x[0]))
+    # 장르 별 많이 재생된 순으로 정렬
+    sorted_dict = sorted(g_dict.items(), key=lambda x:(x[1][0]), reverse=True)
     
-        answer += [i for i, p in sorted_songs[:2]]
+    for genre, (i, play) in sorted_dict:
+        sorted_dict2 = sorted(play, key=lambda x:(-x[1],x[0]))
+        for i, p in sorted_dict2[:2]:
+            answer.append(i)
+        
     
-    
-#     print(g_dict)
     return answer
