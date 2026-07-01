e3752188c5a32d11347803a1922592169cdf42d3
정유경 <jyk4523@gmail.com>
2026-07-01 16:58:43 +0900

    [level 3] Title: 이중우선순위큐, Time: 49.47 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 15.7 MB, 시간: 288.12 ms
+메모리: 20 MB, 시간: 49.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 14일 17:27:08
+2026년 07월 01일 16:58:35
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,30 +1,47 @@
 import heapq
+from collections import Counter
+def clean_max_heap(max_heap, count):
+    while max_heap and count[-max_heap[0]] == 0:
+        heapq.heappop(max_heap)
+ 
+def clean_min_heap(min_heap, count):
+    while min_heap and count[min_heap[0]] == 0:
+        heapq.heappop(min_heap)
+    
 def solution(operations):
     answer = []
     min_heap = []
     max_heap = []
-    heapq.heapify(min_heap)
-    heapq.heapify(max_heap)
+    count = Counter()
     
     for op in operations:
-        ins, num = op.split()
+        order, num = op.split(" ")
         num = int(num)
-        if ins == 'I':
-            heapq.heappush(min_heap, num)
-            heapq.heappush(max_heap, -num) # 최대 힙 구현용
-        elif ins == 'D':
-            if not min_heap:
-                continue
+        if(order == 'I'):
+            # 큐에 주어진 숫자 삽입
+            heapq.heappush(min_heap,num)
+            heapq.heappush(max_heap,-num)
+            count[num] += 1
+        elif order == 'D':
             if num == 1:
-                max_val = -heapq.heappop(max_heap)
-                min_heap.remove(max_val)
+                # 큐에서 최댓값 삭제
+                clean_max_heap(max_heap, count)
+                if max_heap:
+                    x = -heapq.heappop(max_heap)
+                    count[x] -= 1
             elif num == -1:
-                min_val = heapq.heappop(min_heap)
-                max_heap.remove(-min_val)
-            
-    if not min_heap:
-        answer = [0,0]
-    else:
-        answer = [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
-            
-    return answer
+                # 큐에서 최솟값 삭제
+                clean_min_heap(min_heap, count)
+                
+                if min_heap:
+                    x = heapq.heappop(min_heap)
+                    count[x] -= 1
+                    
+    # 마지막 결과 확인 전에도 무효값 정리
+    clean_min_heap(min_heap, count)
+    clean_max_heap(max_heap, count)
+
+    if not min_heap and not max_heap:
+        return [0,0]
+    
+    return [-max_heap[0], min_heap[0]]
