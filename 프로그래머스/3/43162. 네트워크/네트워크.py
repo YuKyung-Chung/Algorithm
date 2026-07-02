7bda87f8f2c317dec68d012fdb3c7323bbed7951
정유경 <jyk4523@gmail.com>
2026-07-02 18:56:50 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,13 @@
 def solution(n, computers):
     answer = 0
-    visited = [False]*n
+    visited = [False] * n
     
     def dfs(node):
-        nonlocal answer
         visited[node] = True
         
         for i in range(n):
             if computers[node][i] == 1 and not visited[i]:
                 dfs(i)
-                
     
     for i in range(n):
         if not visited[i]:
