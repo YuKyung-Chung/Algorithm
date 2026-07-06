23a5b288fabae8938a04de6fd477acaa6b8d0bfe
정유경 <jyk4523@gmail.com>
2026-07-06 21:58:52 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,19 @@
 def solution(n, computers):
     answer = 0
-    visited = [False] * n
+    visited = [False] * (n)
     
     def dfs(node):
         visited[node] = True
         
-        for i in range(n):
-            if computers[node][i] == 1 and not visited[i]:
-                dfs(i)
+        for next_node in range(n):
+            # 연결되어 있고, 아직 방문하지 않았다면 방문
+            if computers[node][next_node] == 1 and not visited[next_node]:
+                dfs(next_node)
     
     for i in range(n):
         if not visited[i]:
-            dfs(i)
             answer += 1
+            dfs(i)
+    
     
     return answer
